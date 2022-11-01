73b88773d8518fad686e7a80235e364afc56c988
nadiah2323 <114987104+nadiah2323@users.noreply.github.com>
2022-11-01 10:13:20 +0000

    Add files via upload

---
+++ AbstractBankAccount.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaceabstract;
+
+abstract class AbstractBankAccount {
+    
+    public final String BANK = "JavaBank Indonesia";
+    
+    protected String accountName;
+    protected String accountNum;
+    protected int balance;
+    
+    public AbstractBankAccount(String name, String num, int balance){
+        this.accountName = name;
+        this.accountNum = num;
+        this.balance = balance;
+    }
+
+    abstract public void deposit(int amt);
+    abstract public int getBalance();
+    abstract public String getBankName();
+    
+    public void withdraw(int amt){}
+    public void setAccountName(String name){
+        this.accountName = name;
+    }
+}
---
+++ InterfaceBankAccount.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaceabstract;
+
+interface InterfaceBankAccount {
+    public final String BANK = "JavaBank Indonesia";
+    
+    public void deposit(int amt);
+    public void withdraw(int amt);
+    public int getBalance();
+    public String getBankName();
+}
